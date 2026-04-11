728ab539235a70f2a585f8b59965973d74719d9c
Zvetozar <ernazar1609gmail.com>
2026-04-11 19:13:01 +0500

    fix files

--- clock/1.py
+++ clock/1.py
@@ -18,20 +18,19 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    current_time = time.localtime()                                                                           #күнінің уақытын аламыз
+    current_time = time.localtime()                                                                          
     minute = current_time.tm_min
     second = current_time.tm_sec
     
-    minute_angle = minute * 6    + (second / 60) * 6                                                         #қазіргі минут * 360 градус / 60 минут + қазіргі секундты қосамыз 
+    minute_angle = minute * 6    + (second / 60) * 6                                                         
     second_angle = second * 6  
     
-    screen.blit(main, (0,0))                                                                                 #основаға суретті орналастыру
-    
-    rotated_rightarm = pygame.transform.rotate(pygame.transform.scale(right, (800, 600)), -minute_angle)     #оң қол минутты орналастыру
+    screen.blit(main, (0,0))                                                                                 
+    rotated_rightarm = pygame.transform.rotate(pygame.transform.scale(right, (800, 600)), -minute_angle)    
     rightarmrect = rotated_rightarm.get_rect(center=(800 // 2 - 30, 600 // 2 - 15))
     screen.blit(rotated_rightarm, rightarmrect)
 
-    rotated_leftarm = pygame.transform.rotate(pygame.transform.scale(left, (40.95, 682.5)), -second_angle)   #сол қол секундты орналастыру
+    rotated_leftarm = pygame.transform.rotate(pygame.transform.scale(left, (40.95, 682.5)), -second_angle)  
     leftarmrect = rotated_leftarm.get_rect(center=(800 // 2 + 20, 600 // 2 - 18))
     screen.blit(rotated_leftarm, leftarmrect)
     
